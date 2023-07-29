a0261f1de0eac1c635b3f497609f95009f5e86d8
mmadyda <marekmadyda@gmail.com>
2023-07-29 12:58:06 +0200

    add bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+var JQuery = require("jquery")
+
+global.$ = global.jQuery = jQuery;
+window.$ = window.jQuery = jQuery;
+
+require('bootstrap')
